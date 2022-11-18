480870fbc36879ead1dace91405b9aafff636ffc
ellilachen <ellilachen@tencent.com>
2022-11-18 18:17:30 +0800

    feat: get all NFTs on the ethereum

--- contract/test/NFT.test.ts
+++ contract/test/NFT.test.ts
@@ -56,7 +56,7 @@ describe("Given ARNFT", function () {
       hash: "00001",
     };
 
-    let tx = await arNFT.awardItem(JSON.stringify(metadata));
+    let tx = await arNFT.connect(owner).awardItem(JSON.stringify(metadata));
     await tx.wait();
 
     const metadata2 = {
@@ -66,12 +66,14 @@ describe("Given ARNFT", function () {
       ar: "https://developer.apple.com/ar/photogrammetry/LemonMeringuePie.usdz",
       hash: "00002",
     };
-    let tx2 = await arNFT.awardItem(JSON.stringify(metadata2));
+    let tx2 = await arNFT
+      .connect(otherAddress)
+      .awardItem(JSON.stringify(metadata2));
     await tx2.wait();
 
     // totalSupply 可以获取所有在链上已经 mint 出的 NFT 数量
     const supplys = await arNFT.totalSupply();
-    console.log("supplys:" + supplys);
+    expect(supplys).to.equal(2);
 
     // ownerOf: tokenId --> address
     let tokenOwner = await arNFT.ownerOf("1");
@@ -83,13 +85,22 @@ describe("Given ARNFT", function () {
 
     // balanceOf: 获取某地址当前拥有的该 NFT 数量
     let count = await arNFT.balanceOf(owner.address);
-    expect(count).to.equal(2);
+    expect(count).to.equal(1);
 
     // 配合 balanceOf 使用, 可以知道这个地址在链上所有的 NFT
     for (let i = 0; i < count.toNumber(); i++) {
+      console.log("==== owner's NFTs =====");
       const tokenId = await arNFT.tokenByIndex(i);
       const data = await arNFT.tokenURI(tokenId);
-      console.log("data=" + data);
+      console.log("index=" + i + ";tokenId=" + tokenId + ";data=" + data);
+    }
+
+    // 配合 totalSupply 使用，可以知道链上所有的 NFT
+    for (let i = 0; i < supplys.toNumber(); i++) {
+      console.log("==== all NFTs =====");
+      const tokenId = await arNFT.tokenByIndex(i);
+      const data = await arNFT.tokenURI(tokenId);
+      console.log("index=" + i + ";tokenId=" + tokenId + ";data=" + data);
     }
   });
 });
